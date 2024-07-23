40a038c3686073f1627641c584381183f4b7b496
Masato Shinokawa <mshinokawa@nvidia.com>
2024-07-23 05:52:27 +0000

    Update the test

--- thunder/tests/test_optim.py
+++ thunder/tests/test_optim.py
@@ -1,42 +1,30 @@
 import torch.testing
 
-from thunder.tests.make_tensor import make_tensor
+from thunder.tests.make_tensor import make_tensor, make_tensor_like
 from thunder.tests.framework import instantiate, NOTHING
-from thunder.torch.optim import adam
+import thunder.torch.optim.adam
 
 
 @instantiate(
     dtypes=NOTHING,
 )
 def test_single_tensor_adam_like(executor, device, _):
-    shape = (1024 * 1024,)
-    params = [make_tensor(shape, device=device, dtype=torch.float32, high=2, low=1) for _ in range(2)]
-    tensors = [
-        [make_tensor(shape, device=device, dtype=torch.float32, high=2, low=1) for _ in range(2)] for _ in range(4)
-    ]
-    tensors = [params] + tensors
-    state_steps = [torch.tensor(1, device=device) for _ in range(2)]
+    shape = (8, 8)
 
-    kwargs = {
-        "grad_scale": None,
-        "found_inf": None,
-        "amsgrad": False,
-        "has_complex": False,
-        "beta1": 0.9,
-        "beta2": 0.999,
-        "lr": 0.001,
-        "weight_decay": 0,
-        "eps": 1e-8,
-        "maximize": False,
-        "capturable": False,
-        "differentiable": False,
-    }
+    params = [make_tensor(shape, device=device, dtype=torch.float32, requires_grad=True) for _ in range(2)]
+    ref_params = [param.clone().detach().requires_grad_(True) for param in params]
 
-    ref_tensors = [[t.clone().detach() for t in tensorlist] for tensorlist in tensors]
-    ref_state_steps = [torch.tensor(1, device=device) for _ in range(2)]
-    adam._single_tensor_adam(*ref_tensors, ref_state_steps, **kwargs)
+    for param, ref_param in zip(params, ref_params):
+        param.grad = make_tensor_like(param)
+        ref_param.grad = param.grad.clone().detach()
 
-    jitted = executor.make_callable(adam._single_tensor_adam)
+    ref_adam = torch.optim.Adam(ref_params, foreach=False)
+    ref_adam.step()
+    ref_adam.step()
 
-    jitted(*tensors, state_steps, **kwargs)
-    torch.testing.assert_close(actual=tensors + [state_steps], expected=ref_tensors + [ref_state_steps])
+    adam = thunder.torch.optim.adam.Adam(params, foreach=False)
+    jitted_step = executor.make_callable(adam.step)
+    jitted_step()
+    jitted_step()
+
+    torch.testing.assert_close(actual=params, expected=ref_params)
