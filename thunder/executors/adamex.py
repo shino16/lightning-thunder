5fdc7107bf4276fb3db24443bf2cecbf8e785708
Masato Shinokawa <mshinokawa@nvidia.com>
2024-07-29 04:42:17 +0000

    adam_step is not an operator itself

--- thunder/executors/adamex.py
+++ thunder/executors/adamex.py
@@ -2,6 +2,7 @@ import torch
 
 from thunder.extend import OperatorExecutor, register_executor
 import thunder.torch.optim.adam
+from thunder.core.proxies import TensorProxy
 
 
 def _adam_step_meta(optim):
@@ -11,32 +12,87 @@ def _adam_step_meta(optim):
 adam_ex = thunder.extend.OperatorExecutor("adam_ex")
 register_executor(adam_ex)
 
-adam_step = adam_ex.register_operator(
-    "adam_step",
-    meta=_adam_step_meta,
-    fn=thunder.torch.optim.adam.step,
-    tags=(thunder.prims.OpTags.DONT_DCE,),
-)
 
-
-def _adam_step_checker(optim):
-    if getattr(optim, "grad_scale", None) is not None or getattr(optim, "found_inf", None) is not None:
+def _adam_checker(
+    params: list[TensorProxy],
+    grads: list[TensorProxy],
+    exp_avgs: list[TensorProxy],
+    exp_avg_sqs: list[TensorProxy],
+    max_exp_avg_sqs: list[TensorProxy],
+    state_steps: list[TensorProxy],
+    foreach: None | bool = None,
+    capturable: bool = False,
+    differentiable: bool = False,
+    fused: None | bool = None,
+    grad_scale: None | TensorProxy = None,
+    found_inf: None | TensorProxy = None,
+    has_complex: bool = False,
+    *,
+    amsgrad: bool,
+    beta1: float,
+    beta2: float,
+    lr: float | TensorProxy,
+    weight_decay: float,
+    eps: float,
+    maximize: bool,
+):
+    if grad_scale is not None or found_inf is not None:
+        return False
+    if any(param.is_complex() for param in params):
+        return False
+    if capturable or differentiable or amsgrad:
         return False
 
-    for group in optim.param_groups:
-        if group["foreach"] or group["fused"]:
-            return False
-        if group["capturable"] or group["differentiable"] or group["amsgrad"]:
-            return False
     return True
 
 
-def _adam_step_transform(optim):
-    adam_step(optim)
+def _adam_transform(
+    params: list[TensorProxy],
+    grads: list[TensorProxy],
+    exp_avgs: list[TensorProxy],
+    exp_avg_sqs: list[TensorProxy],
+    max_exp_avg_sqs: list[TensorProxy],
+    state_steps: list[TensorProxy],
+    foreach: None | bool = None,
+    capturable: bool = False,
+    differentiable: bool = False,
+    fused: None | bool = None,
+    grad_scale: None | TensorProxy = None,
+    found_inf: None | TensorProxy = None,
+    has_complex: bool = False,
+    *,
+    amsgrad: bool,
+    beta1: float,
+    beta2: float,
+    lr: float | TensorProxy,
+    weight_decay: float,
+    eps: float,
+    maximize: bool,
+):
+    thunder.torch.optim.adam._single_tensor_adam(
+        params,
+        grads,
+        exp_avgs,
+        exp_avg_sqs,
+        max_exp_avg_sqs,
+        state_steps,
+        amsgrad=amsgrad,
+        has_complex=has_complex,
+        beta1=beta1,
+        beta2=beta2,
+        lr=lr,
+        weight_decay=weight_decay,
+        eps=eps,
+        maximize=maximize,
+        capturable=capturable,
+        differentiable=differentiable,
+        grad_scale=grad_scale,
+        found_inf=found_inf,
+    )
 
 
 adam_ex.register_implementation(
-    thunder.torch._adam_step,
-    checker=_adam_step_checker,
-    execution_transform=_adam_step_transform,
+    thunder.torch._adam,
+    checker=_adam_checker,
+    execution_transform=_adam_transform,
 )
