aad063e867bbc22f2b5ceba8c88d46e1d73112ad
Masato Shinokawa <mshinokawa@viking-prod-235.ipp2u1.colossus.nvidia.com>
2025-09-18 02:50:37 -0700

    Add model config and bench.sh

--- bench.sh
+++ bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python thunder/benchmarks/benchmark_litgpt.py --model_name Gemma-2-27b --compile dynamo_thunder --checkpoint_activations True --max_iters 1 --warmup_iters 0
+python thunder/benchmarks/benchmark_litgpt.py --model_name Gemma-2-27b-like --compile dynamo_thunder --checkpoint_activations True --max_iters 1 --warmup_iters 0
